bf9d383dcbe755b66674f4c6a164bac96923cc1a
Thomas Norris <tom.norris135@gmail.com>
2021-08-23 14:59:43 -0400

    add mysql; logic to log events

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ var _app = _express();
 const CFG_FILE = _path.resolve(__dirname, 'config', 'config.json');
 var _cfg = readJson(CFG_FILE);
 
+var _mysql = require('mysql');
+var _pool = _mysql.createPool(_cfg.sql.connection);
+
 _app.use(_bodyParser.json());
 
+// POST endpoint
 _app.post(_cfg.express.endpoint, (req, res) => {
     // authenticated?
     if (!authenticated(req.headers)) {
@@ -24,7 +28,7 @@ _app.post(_cfg.express.endpoint, (req, res) => {
         res.status(409).send(msg);
     }
     // valid?
-    else if (!validatePayload(req.body)) {
+    else if (!payloadValid(req.body)) {
         var msg = 'Invalid payload given';
         _logger.Error.Async(msg, 'Payload: ' + JSON.stringify(req.body));
         res.status(409).json({
@@ -34,7 +38,25 @@ _app.post(_cfg.express.endpoint, (req, res) => {
     }
     // do the log
     else {
+        (async (res) => {
+            executeLog(req.body)
+                .then((resp) => {
+                    if (_cfg.debug_mode)
+                        res.status(200).send(resp);
+                    else
+                        res.status(200).send('Log successful.');
 
+                    _logger.Info.Async('Logged REST request');
+                })
+                .catch((err) => {
+                    if (_cfg.debug_mode)
+                        res.status(500).send(err);
+                    else
+                        res.status(500).send('Log unsuccessful.');
+
+                    _logger.Error.Async('Failed to log REST request');
+                });
+        })(res);
     }
 });
 
@@ -54,23 +76,42 @@ function authenticated(headers) {
     });
 }
 
-function validatePayload(payload) {
+function payloadValid(payload) {
     var valid = true;
     Object.keys(_cfg.express.payload_format).forEach((key1) => {
         if (valid) {
             valid = Object.keys(payload).some((key2) => {
-                if (_cfg.express.payload_format[key1].required) {
-                    if (key1.toLowerCase() === key2.toLowerCase())
-                        return true;
-                    else
-                        return false;
-                }
+                if (_cfg.express.payload_format[key1].required)
+                    return key1 === key2;
                 else
                     return true;
             });
         }
     });
     return valid;
+}
+
+// modified version of executeLog from the Node-Logger submodule
+function executeLog(payload) {
+    return new Promise((resolve, reject) => {
+        _pool.getConnection((err, connection) => {
+            if (err)
+                resolve(err);
+            else {
+                var query = 'call ' + _cfg.sql.connection.database + '.' + _cfg.sql.sp.log_rest_request + '(';
+                query += connection.escape(payload.app_ID || _cfg.defaults.app_ID) + ', ' + connection.escape(payload.log_type_ID || _cfg.defaults.log_type_ID) + ', ';
+                query += connection.escape(payload.message || _cfg.defaults.message) + ', ' + connection.escape(payload.details)  + ');';
+
+                connection.query(query, (err, res, fields) => {
+                    connection.release();
+                    if (err)
+                        resolve(err);
+                    else
+                        resolve(res);
+                });
+            }
+        });
+    });
 }
 
 function readJson(filePath) {
